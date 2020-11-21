e6a9580bd05e95fcd45f9e63d01d77301e40a02e
wavem <fenix24@naver.com>
2020-11-21 21:52:27 +0900

    [New] Basic Work Step 01
    - 기초 공사
    - 노트북 페이지 및 Main Form UI 등등
    - 무튼, 그런 기초공사 첫 단계임. 끝.

--- Tetris_Simulator/Main.h
+++ Tetris_Simulator/Main.h
@@ -7,15 +7,102 @@
 #include <Vcl.Controls.hpp>
 #include <Vcl.StdCtrls.hpp>
 #include <Vcl.Forms.hpp>
+#include "cxClasses.hpp"
+#include "cxControls.hpp"
+#include "cxGraphics.hpp"
+#include "cxLookAndFeelPainters.hpp"
+#include "cxLookAndFeels.hpp"
+#include "dxBar.hpp"
+#include "dxRibbon.hpp"
+#include "dxRibbonCustomizationForm.hpp"
+#include "dxRibbonSkins.hpp"
+#include "dxSkinBlack.hpp"
+#include "dxSkinBlue.hpp"
+#include "dxSkinBlueprint.hpp"
+#include "dxSkinCaramel.hpp"
+#include "dxSkinCoffee.hpp"
+#include "dxSkinDarkRoom.hpp"
+#include "dxSkinDarkSide.hpp"
+#include "dxSkinDevExpressDarkStyle.hpp"
+#include "dxSkinDevExpressStyle.hpp"
+#include "dxSkinFoggy.hpp"
+#include "dxSkinGlassOceans.hpp"
+#include "dxSkinHighContrast.hpp"
+#include "dxSkiniMaginary.hpp"
+#include "dxSkinLilian.hpp"
+#include "dxSkinLiquidSky.hpp"
+#include "dxSkinLondonLiquidSky.hpp"
+#include "dxSkinMcSkin.hpp"
+#include "dxSkinMetropolis.hpp"
+#include "dxSkinMetropolisDark.hpp"
+#include "dxSkinMoneyTwins.hpp"
+#include "dxSkinOffice2007Black.hpp"
+#include "dxSkinOffice2007Blue.hpp"
+#include "dxSkinOffice2007Green.hpp"
+#include "dxSkinOffice2007Pink.hpp"
+#include "dxSkinOffice2007Silver.hpp"
+#include "dxSkinOffice2010Black.hpp"
+#include "dxSkinOffice2010Blue.hpp"
+#include "dxSkinOffice2010Silver.hpp"
+#include "dxSkinOffice2013DarkGray.hpp"
+#include "dxSkinOffice2013LightGray.hpp"
+#include "dxSkinOffice2013White.hpp"
+#include "dxSkinPumpkin.hpp"
+#include "dxSkinsCore.hpp"
+#include "dxSkinsDefaultPainters.hpp"
+#include "dxSkinsdxBarPainter.hpp"
+#include "dxSkinsdxRibbonPainter.hpp"
+#include "dxSkinSeven.hpp"
+#include "dxSkinSevenClassic.hpp"
+#include "dxSkinSharp.hpp"
+#include "dxSkinSharpPlus.hpp"
+#include "dxSkinSilver.hpp"
+#include "dxSkinSpringTime.hpp"
+#include "dxSkinStardust.hpp"
+#include "dxSkinSummer2008.hpp"
+#include "dxSkinTheAsphaltWorld.hpp"
+#include "dxSkinValentine.hpp"
+#include "dxSkinVisualStudio2013Blue.hpp"
+#include "dxSkinVisualStudio2013Dark.hpp"
+#include "dxSkinVisualStudio2013Light.hpp"
+#include "dxSkinVS2010.hpp"
+#include "dxSkinWhiteprint.hpp"
+#include "dxSkinXmas2008Blue.hpp"
+#include <Vcl.ExtCtrls.hpp>
+#include "AdvMemo.hpp"
 //---------------------------------------------------------------------------
-class TForm1 : public TForm
+class TFormMain : public TForm
 {
 __published:	// IDE-managed Components
+	TdxRibbon *RibbonMenu;
+	TdxRibbonTab *RibbonMenuTab_Main;
+	TdxBarManager *BarMgr;
+	TdxBar *BarMgrBar1;
+	TdxBarLargeButton *MenuBtn_View;
+	TdxBarLargeButton *MenuBtn_Setting;
+	TdxBarLargeButton *dxBarLargeButton3;
+	TPanel *__pnBase;
+	TNotebook *Notebook_Main;
+	TPanel *_pnBase_01_Main;
+	TPanel *_pnBase_02_Setting;
+	TdxBar *BarMgrBar2;
+	TdxBarLargeButton *MenuBtn_Version;
+	TPanel *pnBase_Status;
+	TAdvMemo *memo;
+	TAdvMemo *AdvMemo1;
+	void __fastcall FormClose(TObject *Sender, TCloseAction &Action);
+	void __fastcall MenuBtn_ViewClick(TObject *Sender);
+	void __fastcall MenuBtn_SettingClick(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
-	__fastcall TForm1(TComponent* Owner);
+	__fastcall TFormMain(TComponent* Owner);
+
+public: // START MJW
+	void __fastcall InitProgram();
+	void __fastcall ExitProgram();
+	void __fastcall PrintMsg(UnicodeString _str);
 };
 //---------------------------------------------------------------------------
-extern PACKAGE TForm1 *Form1;
+extern PACKAGE TFormMain *FormMain;
 //---------------------------------------------------------------------------
 #endif
